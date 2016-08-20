e49513d97006ef73ea007037b7056e7562f3a76c
Ingo Fahrentholz <ingof.work@gmail.com>
2016-08-20 19:12:54 +0200

    Implement Admin mode

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,106 +1,174 @@
 $(function() {
 
-  var controller = {
-
-    init: function() {
-      model.currentCat = model.cats[0];
-      catView.init();
-      catListView.init();
-    },
-
-
-    getAllCats: function() {
-      return model.cats;
-    },
-
-
-    setCurrentCat: function(cat) {
-      return model.currentCat = cat;
-    },
-
-
-    counterIncrement: function() {
-      model.currentCat.clickCount++;
-      return catView.render();
-    }
-
-  };
-
-
   var model = {
     currentCat: null,
+    showAdmin: false,
     cats: [
       {
-        name: "Ingo Cat",
-        imgSrc: "https://placeholdit.imgix.net/~text?txtsize=38&txt=Ingo&w=350&h=150",
-        clickCount: 0
+        name: "Ingo kitty",
+        img: 'http://placeholdit.imgix.net/~text?txtsize=33&txt=350×150&w=350&h=150&bg=ff00ff',
+        count: 0
       },
       {
-        name: "Claudi Cat",
-        imgSrc: "https://placeholdit.imgix.net/~text?txtsize=38&txt=Claudi&w=350&h=150",
-        clickCount: 0
+        name: "Claudi kitty",
+        img: 'http://placeholdit.imgix.net/~text?txtsize=33&txt=350×150&w=350&h=150&bg=ff00ff',
+        count: 0
       },
       {
-        name: "Xenia Cat",
-        imgSrc: "https://placeholdit.imgix.net/~text?txtsize=38&txt=Xenia&w=350&h=150",
-        clickCount: 0
+        name: "Bad kitty",
+        img: 'http://placeholdit.imgix.net/~text?txtsize=33&txt=350×150&w=350&h=150&bg=ff00ff',
+        count: 0
+      },
+      {
+        name: "Good kitty",
+        img: 'http://placeholdit.imgix.net/~text?txtsize=33&txt=350×150&w=350&h=150&bg=ff00ff',
+        count: 0
       }
     ]
   };
 
 
+  var catListView = {
+    init: function() {
+      this.catList = $("#catList");
+      this.render();
+    },
+
+    render: function() {
+      var cat, li, i;
+      var cats = controller.getAllCats();
+      this.catList.empty();
+      for(i = 0; i < cats.length; i++) {
+        cat = cats[i];
+        li = document.createElement('li');
+        li.innerText = cat.name;
+        li.addEventListener('click', (function(catCopy) {
+          return function() {
+            controller.setCurrentCat(catCopy);
+          }
+        })(cat));
+        this.catList.append(li);
+      }
+    }
+  };
+
+
   var catView = {
     init: function() {
-      this.nameNode = document.getElementById("name");
-      this.imageNode = document.getElementById("image");
-      this.counterNode = document.getElementById("counter");
+      this.nameNode = document.getElementById('catName');
+      this.imgNode = document.getElementById('catImg');
+      this.countNode = document.getElementById('catClickCount');
 
-      this.imageNode.addEventListener('click', function() {
-        controller.counterIncrement();
+      this.imgNode.addEventListener('click', function() {
+        controller.incrementCount();
       });
 
       this.render();
     },
 
-
     render: function() {
-      this.nameNode.textContent = model.currentCat.name;
-      this.imageNode.src = model.currentCat.imgSrc;
-      this.counterNode.textContent = model.currentCat.clickCount;
+      var cat = controller.getCurrentCat();
+      this.nameNode.textContent = cat.name;
+      this.imgNode.src = cat.img;
+      this.countNode.textContent = cat.count;
     }
   };
 
-
-  var catListView = {
+  var adminView = {
     init: function() {
-      this.listNode = document.getElementById('list');
-      this.render();
+      var self = this;
+      var currentCat = controller.getCurrentCat();
+      this.editButton = document.getElementById('editCat');
+      this.adminView = document.getElementById('adminView');
+      this.anameNode = document.getElementById('aname');
+      this.aimgNode = document.getElementById('aimg');
+      this.acountNode = document.getElementById('acount');
+      this.saveBtn = document.getElementById('save');
+      this.cancelBtn = document.getElementById('cancel');
+
+      this.cancelBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        controller.setAdminView(false);
+      });
+
+      this.editButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        controller.setAdminView(true);
+      });
+
+      this.saveBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        var newCat = {
+          name: self.anameNode.value,
+          img: self.aimgNode.value,
+          count: self.acountNode.value
+        };
+        controller.updateCat(currentCat, newCat);
+      });
     },
 
+    update: function() {
+      var cat = controller.getCurrentCat();
+      this.anameNode.value = cat.name;
+      this.aimgNode.value = cat.img;
+      this.acountNode.value = cat.count;
+    },
 
     render: function() {
-      var cat, elem, i;
-      var cats = controller.getAllCats();
+      this.adminView.className = '';
+    },
 
-      this.listNode.innerHTML = '';
-
-      for(i = 0; i < cats.length; i++) {
-        cat = cats[i];
-        elem = document.createElement('li');
-        elem.textContent = cat.name;
-
-        elem.addEventListener('click', (function(catClone) {
-          return function() {
-            controller.setCurrentCat(catClone);
-            catView.render();
-          }
-        })(cat));
-
-        this.listNode.appendChild(elem);
-      }
+    hide: function() {
+      this.adminView.className = 'hidden';
     }
   };
 
+
+  var controller = {
+    init: function() {
+      model.currentCat = model.cats[0];
+      catListView.init();
+      catView.init();
+      return adminView.init();
+    },
+
+    incrementCount: function() {
+      model.currentCat.count++;
+      return catView.render();
+    },
+
+    setAdminView: function(bool) {
+      model.showAdmin = bool;
+      if(model.showAdmin) {
+        adminView.render();
+      } else {
+        adminView.hide();
+      }
+    },
+
+    updateCat: function(oldCat, newCat) {
+      var currentCat = controller.getCurrentCat();
+      model.cats = model.cats.map(function(item) {
+        return item.name === currentCat.name ? newCat : item;
+      });
+      controller.setCurrentCat(newCat);
+      catListView.render();
+    },
+
+    setCurrentCat: function(cat) {
+      model.currentCat = cat;
+      catView.render();
+      adminView.update();
+    },
+
+    getCurrentCat: function() {
+      return model.currentCat;
+    },
+
+    getAllCats: function() {
+      return model.cats;
+    }
+  };
 
   controller.init();
 
